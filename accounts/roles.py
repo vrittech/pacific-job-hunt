772622aec9acda31bca8f5e7b4ef81dd6e003fcd
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-27 00:22:42 +0545

    project initialized

---
+++ accounts/roles.py
@@ -0,0 +1,7 @@
+SUPER_ADMIN = 1
+ADMIN = 2
+ENTREPRENEUR = 3
+USER = 5
+
+roles_data = ((SUPER_ADMIN,'SUPER_ADMIN'),(ADMIN,'ADMIN'),(ENTREPRENEUR,'ENTREPRENEUR'),(USER,'USER'))
+roles_data_dict = dict(roles_data)
