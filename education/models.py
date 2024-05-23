f95b8e3f89320afc022d2004317090b2bf1c68bf
vrittech-manoj <manoj@vrittechnologies.com>
2024-05-23 12:35:54 +0545

    .

--- education/models.py
+++ education/models.py
@@ -6,7 +6,6 @@ class Education(models.Model):
     college = models.CharField(max_length = 2000)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="educations")
     degree_name = models.CharField(max_length = 1000)  # in years
-    company_name  = models.CharField(max_length = 1500)
     education_level = models.CharField(max_length = 1000)
     joined_date = models.DateField(null = True)
     end_date  = models.DateField(null = True)
