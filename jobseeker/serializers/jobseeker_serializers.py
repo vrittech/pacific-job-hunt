66e8c53ca7f43725c2712fe137db5d7f7164379e
vrittech-manoj <manoj@vrittechnologies.com>
2024-05-24 10:46:17 +0545

    intrest

--- jobseeker/serializers/jobseeker_serializers.py
+++ jobseeker/serializers/jobseeker_serializers.py
@@ -3,6 +3,20 @@ from ..models import ProfessionalInformation
 from job.models import JobCategory
 from professions.models import Profession
 
+import ast
+
+def str_to_list(data,value_to_convert):
+    mutable_data = data.dict()
+    value_to_convert_data = mutable_data[value_to_convert]
+    if type(value_to_convert_data) == list:
+        return mutable_data
+    try:
+        variations = ast.literal_eval(value_to_convert_data)
+        mutable_data[value_to_convert] = variations
+        return mutable_data
+    except ValueError as e:
+        raise serializers.ValidationError({f'{value_to_convert}': str(e)})
+
 
 class InterestSerializers_JobSeekerPublicSerializers(serializers.ModelSerializer):
     class Meta:
@@ -32,6 +46,13 @@ class JobSeekerRetrieveSerializers(serializers.ModelSerializer):
 
 
 class JobSeekerWriteSerializers(serializers.ModelSerializer):
+
+    def to_internal_value(self, data):
+        if data.get('interest'):
+            data = str_to_list(data,'interest')
+            return super().to_internal_value(data)
+        return super().to_internal_value(data)
+
     class Meta:
         model = ProfessionalInformation
         fields = '__all__'
