5bd81564a2468199298264274382b866a5431493
vrittech-manoj <manoj@vrittechnologies.com>
2024-06-11 18:52:46 +0545

    googlelogin

--- accounts/google_virify.py
+++ accounts/google_virify.py
@@ -3,8 +3,9 @@
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
-GOOGLE_CLIENT_ID = ["840477817407-fudv992qklqqln48ggmpk0na5o76ni5n.apps.googleusercontent.com","840477817407-c2slaq8uhp8d5jpdd81plvg960j6aam7.apps.googleusercontent.com"]
+GOOGLE_CLIENT_ID = ["698950373821-j8vpbqpj94ihubrttrgp8rbq2vg78n8e.apps.googleusercontent.com"]
 def VerifyGoogleToken(token):
+    # print(token)
     try:
         idinfo = id_token.verify_oauth2_token(
             token,
