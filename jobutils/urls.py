c4c70630e92f4faf0bdd9a5b6906033f1803ff0e
vrittech-manoj <manoj@vrittechnologies.com>
2024-06-23 12:25:29 +0545

    route for JobTimingViewset,JobLevelViewset,JobLocationViewset,JobPositionViewset

--- jobutils/urls.py
+++ jobutils/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import job_timing_viewsets
+from .viewsets import job_timing_viewsets,job_level_viewsets,job_location_viewsets,job_position_viewsets
 
 router = DefaultRouter()
 
 
-router.register('education-information', job_timing_viewsets.EducationViewset, basename="EducationViewset")
+router.register('job-timing', job_timing_viewsets.JobTimingViewset, basename="JobTimingViewset")
+router.register('job-level', job_level_viewsets.JobLevelViewset, basename="JobLevelViewset")
+router.register('job-location', job_location_viewsets.JobLocationViewset, basename="JobLocationViewset")
+router.register('job-position', job_position_viewsets.JobPositionViewset, basename="JobPositionViewset")
 
 urlpatterns = [    
     path('', include(router.urls)),
