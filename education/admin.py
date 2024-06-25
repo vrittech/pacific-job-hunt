f4ddc1d959b18ff54cc05325f166db91657cd553
vrittech-manoj <manoj@vrittechnologies.com>
2024-06-25 18:22:06 +0545

    eduction level

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Education,EducationLevel
 # Register your models here.
+admin.site.register([Education,EducationLevel])
--- education/models.py
+++ education/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from accounts.models import CustomUser
 
 # Create your models here.
-class Education(models.Model):
-    college = models.CharField(max_length = 2000)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="educations")
-    degree_name = models.CharField(max_length = 1000)  # in years
-    education_level = models.CharField(max_length = 1000)
-    joined_date = models.DateField(null = True)
-    end_date  = models.DateField(null = True)
-    is_currently = models.BooleanField(default = False)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-
-
 class EducationLevel(models.Model):
     name = models.CharField(max_length = 200)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
+
+
+class Education(models.Model):
+    college = models.CharField(max_length = 2000)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="educations")
+    degree_name = models.CharField(max_length = 1000)  # in years
+    education_level = models.ForeignKey(EducationLevel,on_delete = models.SET_NULL,null = True)
+    joined_date = models.DateField(null = True)
+    end_date  = models.DateField(null = True)
+    is_currently = models.BooleanField(default = False)
+    created_date = models.DateTimeField(auto_now_add=True)
