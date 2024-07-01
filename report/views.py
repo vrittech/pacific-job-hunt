09cfbd5465883566912225e06bfbec2798070b16
vrittech-manoj <manoj@vrittechnologies.com>
2024-07-01 17:03:31 +0545

    get sample type

--- report/views.py
+++ report/views.py
@@ -6,6 +6,9 @@ import pandas as pd
 from django.http import HttpResponse
 import csv
 from jobutils.models import JobTiming,JobLocation,JobLevel
+from job.models import JobCategory,Skills
+from professions.models import  Profession
+from company.models import CompanyType
 
 class getSample(APIView):
     def get(self, request, type, format=None):
@@ -16,6 +19,16 @@ class getSample(APIView):
                 queryset = JobLevel.objects.all()
             elif type == "job-location":
                 queryset = JobLocation.objects.all()
+
+            elif type == "job-category":
+                queryset = JobCategory.objects.all()
+            elif type == "profession":
+                queryset = Profession.objects.all()
+            elif type == "skills":
+                queryset = Skills.objects.all()
+            elif type == "company-type":
+                queryset = CompanyType.objects.all()
+     
             else:
                 return Response({"message": 'Unknown type'}, status=status.HTTP_400_BAD_REQUEST)
             
