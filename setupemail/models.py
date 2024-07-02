962889c63251f959b7bc61f83ba45d093674081f
vrittech-manoj <manoj@vrittechnologies.com>
2024-07-02 15:36:47 +0545

    max length in emailsetup

--- setupemail/models.py
+++ setupemail/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class EmailSetup(models.Model):
-    smtp_server_address = models.CharField()  #EMAIL_HOST
-    email_address = models.EmailField()
-    password = models.CharField() #app password
+    smtp_server_address = models.CharField(max_length = 300)  #EMAIL_HOST
+    email_address = models.EmailField(max_length = 300)
+    password = models.CharField(max_length = 2000) #app password
     port = models.PositiveIntegerField()
     required_authentication = models.BooleanField(default = True)
     security = models.CharField(max_length = 200,choices = (('None','None'),('SSL','SSL'),('TSL','TSL')),default = 'None')
